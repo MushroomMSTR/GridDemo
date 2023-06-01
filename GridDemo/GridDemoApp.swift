1f3c0ca9919eb146ae5647f2bb964f17d40fa8b1
NazarStf <hworker98@gmail.com>
2023-06-01 23:10:09 +0300

    Initial Commit

---
+++ GridDemo/GridDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GridDemoApp.swift
+//  GridDemo
+//
+//  Created by NazarStf on 01.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GridDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
